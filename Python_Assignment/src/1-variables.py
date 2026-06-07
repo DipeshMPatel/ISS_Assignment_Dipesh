f98f09c61cdacf2d0fef88d34e4d6579cdf41a49
DipeshMPatel <dipeshpatel.mp@gmail.com>
2026-06-08 00:25:18 +0530

    Variables and Constants

---
+++ Python_Assignment/src/1-variables.py
@@ -0,0 +1,15 @@
+name = "Dipesh Patel"   # String Assignment
+age = 21                # Integer Assignment
+height = 5.11           # Float Assignment
+
+print(f"Name:{name}")
+print(f"Age:{age}")
+print(f"Height:{height}")
+
+
+a, b, c = 1, 2, 3       # Multiple Assignment
+print(f"a:{a}, b:{b}, c:{c}")
+
+a = "Dynamic"           #Does not give any erroe as python is dynamically typed
+print("After Reassignment:")
+print(f"a:{a}")
---
+++ Python_Assignment/src/2-constants.py
@@ -0,0 +1,12 @@
+PI = 3.14159265359
+GRAVITY = 9.8
+
+MAX_ATTEMPTS = 3
+DEFAULT_TIMEOUT = 30
+HF_KEY = "hf_sjdfdkjnckjbuhfnjkdcnhicdsXXX"
+
+print(f"Area of circle with radius of 2cm is {PI} x 2 x 2 =", PI*2*2, "cm^2" )
+print(f"Acceleration due to gravity on earth: {GRAVITY}")
+print(f"Maximum login attempts allowed: {MAX_ATTEMPTS}")
+print(f"Default timeout: {DEFAULT_TIMEOUT}")
+print(f"Hugging Face key: {HF_KEY}")
